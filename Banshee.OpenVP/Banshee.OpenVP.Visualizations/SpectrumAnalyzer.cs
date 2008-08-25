ab515ccb3b63684c922fa6b94d562a17362c7b0e
Chris Howie <cdhowie@gmail.com>
2008-08-25 14:42:36 +0000

    * Banshee.OpenVP/Banshee.OpenVP.Visualizations/SpectrumAnalyzer.cs:   Tweak spectrum analyzer.

--- Banshee.OpenVP/Banshee.OpenVP.Visualizations/SpectrumAnalyzer.cs
+++ Banshee.OpenVP/Banshee.OpenVP.Visualizations/SpectrumAnalyzer.cs
@@ -12,20 +12,22 @@ namespace Banshee.OpenVP.Visualizations
 {
 	public class SpectrumAnalyzer : IRenderer
 	{
-		private const int FREQUENCY_COUNT = 64;
+		private const int FREQUENCY_COUNT = 512;
 
 		private const float SPACING = 2f / FREQUENCY_COUNT;
 
 		private float[] spectrum = new float[FREQUENCY_COUNT];
+
+		private float[] newspec = new float[FREQUENCY_COUNT];
 		
 		public SpectrumAnalyzer()
 		{
 		}
 
-		private void MergeSpectrum(float[] newspec)
+		private void MergeSpectrum()
 		{
 			for (int i = 0; i < FREQUENCY_COUNT; i++) {
-				spectrum[i] = Math.Max(newspec[i], spectrum[i] / 1.25f);
+				this.spectrum[i] = Math.Max(this.newspec[i], this.spectrum[i] / 1.25f);
 			}
 		}
 
@@ -34,12 +36,10 @@ namespace Banshee.OpenVP.Visualizations
 			gl.glClearColor(0, 0, 0, 1);
 			gl.glClear(gl.GL_COLOR_BUFFER_BIT);
 
-			float[] newspec = new float[FREQUENCY_COUNT];
-			controller.PlayerData.GetSpectrum(newspec);
-			this.MergeSpectrum(newspec);
+			controller.PlayerData.GetSpectrum(this.newspec);
+			this.MergeSpectrum();
 			
 			gl.glBegin(gl.GL_QUADS);
-			Color green = new Color(0, 1, 0);
 			
 			for (int i = 0; i < FREQUENCY_COUNT; i++) {
 				Color color = Color.FromHSL(120 * (1 - this.spectrum[i]), 1, 0.5f);
@@ -53,7 +53,6 @@ namespace Banshee.OpenVP.Visualizations
 				gl.glVertex2f(x1, v);
 				gl.glVertex2f(x2, v);
 				
-				green.Use();
 				gl.glVertex2f(x2, -1);
 				gl.glVertex2f(x1, -1);
 			}
