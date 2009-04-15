19ecd9b110abbd6320635f33ab512a046c4474a6
Christian Martellini <christian.martellini@gmail.com>
2009-04-15 15:05:21 +0000

    git-svn-id: http://bansheelyricsplugin.googlecode.com/svn/trunk@148 3876dd14-234a-0410-a5e2-759f519755f9

--- BansheeLyricsPlugin/config.h
+++ BansheeLyricsPlugin/config.h
@@ -2,22 +2,22 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Name of package */
-#define PACKAGE "bansheelyricsplugin"
+#define PACKAGE "BansheeLyricsPlugin"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "BansheeLyricsPlugin"
+#define PACKAGE_NAME ""
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BansheeLyricsPlugin 0.6"
+#define PACKAGE_STRING ""
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "bansheelyricsplugin"
+#define PACKAGE_TARNAME ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.6"
+#define PACKAGE_VERSION ""
 
 /* Version number of package */
-#define VERSION "0.6"
+#define VERSION "1.0"
