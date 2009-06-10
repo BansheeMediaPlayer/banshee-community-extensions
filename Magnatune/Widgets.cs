367f083583f6dad3912b472273f5d74ef3d67445
Max Battcher <me@worldmaker.net>
2009-06-10 22:56:29 +0200

    Explicitly add MIT/X11 license to source files, LICENSE file
    Ignore-this: aed7e76368c899b4ee3f5a093b7dc4cb
    
    darcs-hash:20090610205629-a58ed-228a24d6e0c3b51d8cf9f27d9bf3a5607453c468.gz

--- Magnatune/Widgets.cs
+++ Magnatune/Widgets.cs
@@ -1,7 +1,28 @@
-// Widgets.cs created with MonoDevelop
-// User: worldmaker at 6:27 PM 6/10/2008
-//
-// Widgets based upon LastfmSourceContents
+/*
+Magnatune Plugin for Banshee. Widgets for Gtk based upon Last.fm
+widgets (from LastFmSourceContents).
+
+Copyright 2008 Max Battcher <me@worldmaker.net>.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 using Banshee.Streaming;
 using Banshee.Widgets;
