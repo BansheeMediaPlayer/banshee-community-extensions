169a4f82a67c551dff15f1f4bfeeebdb883799cd
Andreas <dingsi@dingsi.(none)>
2010-01-17 03:11:20 +0700

    initial version 0.2

---
+++ Banshee.Streamripper/Banshee.Streamripper/StreamripperProcess.cs
@@ -0,0 +1,198 @@
+//
+// StreamripperProcess.cs
+//
+// Author:
+//   Frank Ziegler
+//   based on Banshee-Streamripper by Akseli Mantila <aksu@paju.oulu.fi>
+//
+// Copyright (C) 2009 Akseli Mantila
+// Copyright (C) 2009 Frank Ziegler
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+using System.Text.RegularExpressions;
+
+using Mono.Addins;
+
+using Banshee.ServiceStack;
+using Banshee.Collection;
+
+using Hyena;
+
+namespace Banshee.Streamripper
+{
+    public class StreamripperProcess : Process
+    {
+        private string output_file_format_pattern;
+        private string output_directory;
+        private string uri;
+        private bool isogg;
+
+        public StreamripperProcess () 
+        {
+            StartInfo.FileName = "streamripper";
+        }
+        
+        public void StartRecording () 
+        {
+            Hyena.Log.Debug ("[StreamripperProcess] <StartRecording> START");
+            
+            if (!SetParameters ())
+                return;
+          
+            try {
+                Start ();
+            }
+            catch (Exception e) {
+                Hyena.Log.Exception ("[StreamripperProcess] <StartRecording> Couldn't start", e);
+            }
+                
+            Hyena.Log.DebugFormat ("[StreamripperProcess] <StartRecording> Arguments {0}", StartInfo.Arguments);
+            Hyena.Log.Debug ("[StreamripperProcess] <StartRecording> END");
+        }
+
+        public void StopRecording () 
+        {
+            Hyena.Log.Debug ("[StreamripperProcess] <StopRecording> STOPPED");
+
+            try {
+                if (!HasExited)
+                    Kill ();   
+            } catch (Exception e) {
+                Hyena.Log.Exception ("[StreamripperProcess] <StopRecording> Couldn't stop", e);
+            }
+        }
+
+        public bool SetParameters () 
+        {
+            if (String.IsNullOrEmpty (uri))
+                return false;
+
+            StartInfo.Arguments = uri;
+
+            if (!String.IsNullOrEmpty (output_file_format_pattern))
+                StartInfo.Arguments += " -t -D " + output_file_format_pattern + " ";
+
+            if (!String.IsNullOrEmpty (output_directory))
+                StartInfo.Arguments += " -d " + output_directory + " ";
+                
+            if (this.isogg)
+				StartInfo.Arguments += " -A -a ";
+
+            return true;
+        }
+        
+        public void SetOutputFileFormatPattern (string pattern) 
+        {
+            output_file_format_pattern = pattern;
+        }
+
+        public void SetOutputDirectory (string directory) 
+        {
+            output_directory = directory;
+        }
+
+        public void SetStreamURI (string uri) 
+        {
+            this.uri = uri;
+			Regex objOggPattern=new Regex("^.*ogg.*$");
+			if (objOggPattern.IsMatch(uri))
+			{
+				this.isogg = true;
+			}
+			else
+			{
+				this.isogg = false;
+			}
+        }
+    }
+
+    public class MplayerProcess : Process
+    {
+        private string output_file;
+        private string uri;
+
+        public MplayerProcess () 
+        {
+            StartInfo.FileName = "mplayer";
+        }
+        
+        public void StartRecording () 
+        {
+            Hyena.Log.Debug ("[MplayerProcess] <StartRecording> START");
+            
+            if (!SetParameters ())
+                return;
+          
+            try {
+                Start ();
+            }
+            catch (Exception e) {
+                Hyena.Log.Exception ("[MplayerProcess] <StartRecording> Couldn't start", e);
+            }
+                
+            Hyena.Log.DebugFormat ("[MplayerProcess] <StartRecording> Arguments {0}", StartInfo.Arguments);
+            Hyena.Log.Debug ("[MplayerProcess] <StartRecording> END");
+        }
+
+        public void StopRecording () 
+        {
+            Hyena.Log.Debug ("[MplayerProcess] <StopRecording> STOPPED");
+
+            try {
+                if (!HasExited)
+                    Kill ();   
+            } catch (Exception e) {
+                Hyena.Log.Exception ("[MplayerProcess] <StopRecording> Couldn't stop", e);
+            }
+        }
+
+        public bool SetParameters () 
+        {
+            if (String.IsNullOrEmpty (uri))
+                return false;
+
+            StartInfo.Arguments = " -noframedrop -noconfig all -nolirc -slave -dumpstream " + uri;
+
+            if (!String.IsNullOrEmpty (output_file))
+                StartInfo.Arguments += " -dumpfile " + output_file;
+
+            return true;
+        }
+        
+        public void SetOutputFile (string fullfilename) 
+        {
+			Console.WriteLine ("[MplayerProcess] <SetOutputFile> set filename : " + fullfilename );
+            output_file = Regex.Replace(fullfilename, @" ", "_");
+        }
+
+        public void SetStreamURI (string uri) 
+        {
+            this.uri = uri;
+        }
+    }
+
+}
---
+++ Banshee.Streamripper/Banshee.Streamripper/StreamripperService.cs
@@ -0,0 +1,376 @@
+//
+// StreamripperService.cs
+//
+// Author:
+//   Frank Ziegler
+//   based on Banshee-Streamripper by Akseli Mantila <aksu@paju.oulu.fi>
+//
+// Copyright (C) 2009 Akseli Mantila
+// Copyright (C) 2009 Frank Ziegler
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+using Mono.Unix;
+using Mono.Addins;
+
+using Gtk;
+
+using Banshee.ServiceStack;
+using Banshee.Collection;
+using Banshee.Gui;
+using Banshee.Configuration;
+using Banshee.MediaEngine;
+
+using Hyena;
+
+namespace Banshee.Streamripper
+{
+    public class StreamripperService : IExtensionService, IDisposable
+    {
+        private StreamripperProcess streamripper_process = null;
+        private MplayerProcess mplayer_process = null;
+        private ActionGroup actions;
+        private InterfaceActionService action_service;
+        private uint ui_manager_id;
+        private bool recording = false;
+        private string output_directory;
+        private bool is_importing_enabled = true;
+        private bool has_http_header = false;
+		private TrackInfo track = null;
+
+        
+        public StreamripperService ()
+        {
+            Hyena.Log.Debug ("[StreamripperService] <StreamripperService> START");
+            
+            recording = IsRecordingEnabledEntry.Get ().Equals ("True") ? true : false;
+            output_directory = OutputDirectoryEntry.Get ();
+            is_importing_enabled = IsImportingEnabledEntry.Get ().Equals ("True") ? true : false;
+        }
+        
+        void IExtensionService.Initialize ()
+        {
+            Hyena.Log.Debug ("[StreamripperService] <Initialize> START");
+
+            ServiceManager.PlaybackController.TrackStarted += delegate {
+                if (recording) {
+                    StartRecording ();
+                }
+            };
+            
+            ServiceManager.PlaybackController.Stopped += delegate {
+                if (recording) {
+                    StopRecording ();
+                }
+            };
+        
+            ServiceManager.PlayerEngine.ConnectEvent ( OnEndOfStream , PlayerEvent.EndOfStream) ;
+            ServiceManager.PlayerEngine.ConnectEvent ( OnStateChange , PlayerEvent.StateChange) ;
+        
+            action_service = ServiceManager.Get<InterfaceActionService> ("InterfaceActionService");
+            actions = new ActionGroup ("Streamripper");
+            
+            actions.Add (new ActionEntry [] {
+                new ActionEntry ("StreamripperAction", null,
+                    AddinManager.CurrentLocalizer.GetString ("_Streamripper"), null,
+                    null, null),
+
+                new ActionEntry ("StreamripperConfigureAction", Stock.Properties,
+                    AddinManager.CurrentLocalizer.GetString ("_Configure"), null,
+                    AddinManager.CurrentLocalizer.GetString ("Configure the Streamripper plugin"), OnConfigure)
+            });
+                
+            actions.Add (new ToggleActionEntry [] { 
+                new ToggleActionEntry ("StreamripperEnableAction", Stock.MediaRecord,
+                    AddinManager.CurrentLocalizer.GetString ("_Activate streamripper"), null,
+                    AddinManager.CurrentLocalizer.GetString ("Activate streamripper process"), OnActivateStreamripper, recording)
+            });
+
+            action_service.UIManager.InsertActionGroup (actions, 0);
+            ui_manager_id = action_service.UIManager.AddUiFromResource ("Resources.StreamripperMenu.xml");
+
+            Hyena.Log.Debug ("[StreamripperService] <Initialize> END");
+        }
+
+        public void OnActivateStreamripper (object o, EventArgs ea) 
+        {
+            Hyena.Log.Debug ("[StreamripperService] <OnActivateStreamripper> START");
+                    
+            if (!recording) { 
+				StartRecording ();
+            }
+            else {
+                StopRecording ();        
+            }
+
+            recording = !recording;
+            IsRecordingEnabledEntry.Set (recording.ToString ());
+
+            Hyena.Log.Debug ("[StreamripperService] <OnActivateStreamripper> END");
+        }
+        
+        public void OnConfigure (object o, EventArgs ea)
+        {
+            new StreamripperConfigDialog (this, output_directory, is_importing_enabled);
+        }
+            
+        public void Dispose ()
+        {
+            Log.Debug ("Disposing Streamripper plugin");
+
+            StopRecording ();
+            action_service.UIManager.RemoveUi (ui_manager_id);
+            action_service.UIManager.RemoveActionGroup (actions);
+            ServiceManager.PlayerEngine.DisconnectEvent ( OnEndOfStream ) ;
+            ServiceManager.PlayerEngine.DisconnectEvent ( OnStateChange ) ;
+            actions = null;
+        }
+        
+        string IService.ServiceName {
+            get { return "StreamripperService"; }
+        }
+
+        private bool IsCurrentTrackRecordable () 
+        {
+            if (Banshee.ServiceStack.ServiceManager.PlaybackController.CurrentTrack.IsLive 
+				&& Banshee.ServiceStack.ServiceManager.PlaybackController.CurrentTrack.IsPlaying)
+                return true;
+            
+            return false;
+        }
+
+        private void OnEndOfStream (PlayerEventArgs args)
+        {
+            if (recording) {
+                StopRecording ();
+            }
+        }
+
+        private void OnStateChange (PlayerEventArgs args)
+        {
+            //Console.WriteLine ("OnStateChange" );
+            //Console.WriteLine (ServiceManager.PlayerEngine.CurrentState);
+            //Console.WriteLine (ServiceManager.PlayerEngine.LastState);
+            if (ServiceManager.PlayerEngine.CurrentState == PlayerState.Idle && recording) {
+                StopRecording ();
+            }        
+        }
+
+        private void StartRecording () 
+        {
+            if (recording) {
+                StopRecording ();
+            }
+			
+			if (!IsCurrentTrackRecordable ()) {
+				 return;
+			}
+			
+			bool result = false;
+			track = ServiceManager.PlaybackController.CurrentTrack;
+			has_http_header = CheckHttpHeader(track);
+			
+			if (has_http_header)
+			{
+				result = InitStreamripperProcess (track) ;
+			}
+			else
+			{
+				result = InitMplayerProcess (track) ;
+			}
+            if (result) {
+				if (has_http_header)
+				{
+					streamripper_process.StartRecording ();
+				}
+				else
+				{
+					mplayer_process.StartRecording ();
+				}
+                if (is_importing_enabled)
+                    StartFolderScanner ();
+            }
+        }
+
+        private void StopRecording () 
+        {
+			if (has_http_header)
+			{
+				if (streamripper_process != null)
+					streamripper_process.StopRecording ();
+			}
+			else
+			{
+				if (mplayer_process != null)
+					mplayer_process.StopRecording ();
+			}
+
+            StopFolderScanner ();
+        }
+     
+        public void StartFolderScanner () 
+        {
+            RippedFileScanner.StartScanner ();
+        }
+
+        public void StopFolderScanner () 
+        {
+           RippedFileScanner.StopScanner ();
+        }
+                
+        private bool InitStreamripperProcess (TrackInfo track_in) 
+        {
+            Hyena.Log.DebugFormat ("[StreamripperService] <InitStreamripperProcess> START dir: '{0}'", output_directory);
+                    
+            if (String.IsNullOrEmpty (output_directory)) {
+                output_directory = Banshee.ServiceStack.ServiceManager.SourceManager.MusicLibrary.BaseDirectory +
+                    Path.DirectorySeparatorChar + "ripped";
+            }
+                    
+            if (streamripper_process == null)
+                streamripper_process = new StreamripperProcess ();
+            
+            if (track_in == null) {
+                Hyena.Log.Debug ("[StreamripperService] <InitStreamripperProcess> END. Recording not ready");
+                return false;
+            }
+                  
+            if (track_in.Uri == null || track_in.Uri.IsLocalPath) {
+                Hyena.Log.Debug ("[StreamripperService] <InitStreamripperProcess> END. Not recording local files");
+                return false;
+            }
+                  
+            streamripper_process.SetStreamURI (track_in.Uri.ToString ());
+            streamripper_process.SetOutputDirectory (output_directory);
+            streamripper_process.SetOutputFileFormatPattern ("\"%A/%a/%T\"");
+
+            RippedFileScanner.SetScanDirectory (output_directory);
+                    
+            Hyena.Log.Debug ("[StreamripperService] <InitStreamripperProcess> END. Recording ready");
+            return true;
+        }
+                
+        private bool InitMplayerProcess (TrackInfo track_in) 
+        {
+            Hyena.Log.DebugFormat ("[StreamripperService] <InitMplayerProcess> START dir: '{0}'", output_directory);
+                    
+            if (String.IsNullOrEmpty (output_directory)) {
+                output_directory = Banshee.ServiceStack.ServiceManager.SourceManager.MusicLibrary.BaseDirectory +
+                    Path.DirectorySeparatorChar + "ripped";
+            }
+                    
+            if (mplayer_process == null)
+                mplayer_process = new MplayerProcess ();
+            
+            if (track_in == null) {
+                Hyena.Log.Debug ("[StreamripperService] <InitMplayerProcess> END. Recording not ready");
+                return false;
+            }
+			
+            if (track_in.Uri == null || track_in.Uri.IsLocalPath) {
+                Hyena.Log.Debug ("[StreamripperService] <InitStreamripperProcess> END. Not recording local files");
+                return false;
+            }
+                  
+			DateTime dt = DateTime.Now;
+			string datestr = String.Format("{0:d_M_yyyy_HH_mm_ss}", dt);
+			string fileext = Regex.Replace(track.Uri.ToString(), @"^.*(\.[^\.]*)$", "$1");
+
+            mplayer_process.SetStreamURI (track.Uri.ToString ());
+            mplayer_process.SetOutputFile (output_directory + track.TrackTitle + "_" + datestr + fileext);
+
+            RippedFileScanner.SetScanDirectory (output_directory);
+                    
+            Hyena.Log.Debug ("[StreamripperService] <InitMplayerProcess> END. Recording ready");
+            return true;
+        }
+                
+        public string OutputDirectory 
+        {
+            get { return output_directory; }
+            set 
+            {
+                StopRecording ();
+                StopFolderScanner ();
+                    
+                this.output_directory = value;
+               
+                Hyena.Log.DebugFormat ("[StreamripperService] <OutputDirectorySetter> ", value);
+
+				bool result = false;
+				track = ServiceManager.PlaybackController.CurrentTrack;
+				has_http_header = CheckHttpHeader(track);
+				
+				if (has_http_header)
+				{
+					result = InitStreamripperProcess (track) ;
+				}
+				else
+				{
+					result = InitMplayerProcess (track) ;
+				}
+                if (result) {
+                    if (is_importing_enabled)
+                        StartFolderScanner ();
+                    if (recording)
+                        StartRecording ();
+                }
+            }
+        }
+         
+        public bool CheckHttpHeader (TrackInfo track_in)
+        {
+            if (track_in == null) {
+                Hyena.Log.Debug ("[StreamripperService] <InitMplayerProcess> END. Recording not ready");
+                return false;
+            }
+            if (track_in.Uri == null) {
+                Hyena.Log.Debug ("[StreamripperService] <InitMplayerProcess> END. Recording not ready");
+                return false;
+            }
+			return Regex.Match(track_in.Uri.ToString (),"^http://" ).Success;
+        }
+
+        public bool IsImportingEnabled
+        {
+            get { return is_importing_enabled; }
+            set { is_importing_enabled = value; }
+        }
+       
+        public static readonly SchemaEntry<string> IsRecordingEnabledEntry = new SchemaEntry<string> (
+            "plugins.streamripper", "is_recording_enabled", "", "Is ripping enabled", "Is ripping enabled"
+        );
+                
+        public static readonly SchemaEntry<string> OutputDirectoryEntry = new SchemaEntry<string> (
+            "plugins.streamripper", "output_directory", "", "Output directory for ripped files", 
+            "Output directory for ripped files"
+        );
+                
+        public static readonly SchemaEntry<string> IsImportingEnabledEntry = new SchemaEntry<string> (
+            "plugins.streamripper", "is_importing_enabled", "", "Is importing enabled", "Is importing enabled"
+        );
+    }
+}
