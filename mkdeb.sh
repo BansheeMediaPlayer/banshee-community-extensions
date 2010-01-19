f3f43c64cee741e3dda357e0b989bfee8e523706
Frank Ziegler <funtastix@googlemail.com>
2010-01-19 10:51:47 +0700

    fixed packaging script
    Signed-off-by: Frank Ziegler <funtastix@googlemail.com>

--- mkdeb.sh
+++ mkdeb.sh
@@ -9,15 +9,15 @@ copyfiles ()
 	mkdir -p debian/usr/share/locale/en/LC_MESSAGES
 	mkdir -p debian/usr/share/locale/de/LC_MESSAGES
 
-	cp /usr/lib/banshee-1/Extensions/Banshee.Streamrecorder.dll debian/usr/lib/banshee-1/Extensions/Banshee.Streamrecorder.dll
-	cp /usr/share/doc/banshee-extension-streamrecorder/AUTHORS debian/usr/share/doc/banshee-extension-streamrecorder/AUTHORS
-	cp /usr/share/doc/banshee-extension-streamrecorder/changelog.Debian.gz debian/usr/share/doc/banshee-extension-streamrecorder/changelog.Debian.gz
-	cp /usr/share/doc/banshee-extension-streamrecorder/copyright debian/usr/share/doc/banshee-extension-streamrecorder/copyright
-	cp /usr/share/doc/banshee-extension-streamrecorder/NEWS.gz debian/usr/share/doc/banshee-extension-streamrecorder/NEWS.gz
-	cp /usr/share/doc/banshee-extension-streamrecorder/README debian/usr/share/doc/banshee-extension-streamrecorder/README
-	cp /usr/share/locale/de/LC_MESSAGES/banshee-streamrecorder.mo debian/usr/share/locale/de/LC_MESSAGES/banshee-streamrecorder.mo
-	cp /usr/share/locale/en/LC_MESSAGES/banshee-streamrecorder.mo debian/usr/share/locale/en/LC_MESSAGES/banshee-streamrecorder.mo
-	cp /usr/share/locale/fi/LC_MESSAGES/banshee-streamrecorder.mo debian/usr/share/locale/fi/LC_MESSAGES/banshee-streamrecorder.mo
+	cp Banshee.Streamrecorder/bin/Release/Banshee.Streamrecorder.dll Banshee.Streamrecorder.dll debian/usr/lib/banshee-1/Extensions/Banshee.Streamrecorder.dll
+	#cp /usr/share/doc/banshee-extension-streamrecorder/AUTHORS debian/usr/share/doc/banshee-extension-streamrecorder/AUTHORS
+	#cp /usr/share/doc/banshee-extension-streamrecorder/changelog.Debian.gz debian/usr/share/doc/banshee-extension-streamrecorder/changelog.Debian.gz
+	#cp /usr/share/doc/banshee-extension-streamrecorder/copyright debian/usr/share/doc/banshee-extension-streamrecorder/copyright
+	#cp /usr/share/doc/banshee-extension-streamrecorder/NEWS.gz debian/usr/share/doc/banshee-extension-streamrecorder/NEWS.gz
+	#cp /usr/share/doc/banshee-extension-streamrecorder/README debian/usr/share/doc/banshee-extension-streamrecorder/README
+	cp po/de.gmo debian/usr/share/locale/de/LC_MESSAGES/banshee-streamrecorder.mo
+	cp po/en.gmo debian/usr/share/locale/en/LC_MESSAGES/banshee-streamrecorder.mo
+	cp po/fi.gmo debian/usr/share/locale/fi/LC_MESSAGES/banshee-streamrecorder.mo
 }
 
 makedeb ()
