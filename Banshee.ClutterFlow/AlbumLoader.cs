6cf388fc429cdfde5d64b942f6b7758311fe30b0
Mathijs Dumon <mathijsken@hotmail.com>
2010-01-30 15:05:15 +0000

    * AlbumLoader.cs: Converting tabs to 4 spaces, added check to   disable reloading when model count is the same
    git-svn-id: https://clutterflow.svn.sourceforge.net/svnroot/clutterflow/trunk@40 df4caec8-3d98-4697-9cf0-ec62fe0c6711

--- Banshee.ClutterFlow/AlbumLoader.cs
+++ Banshee.ClutterFlow/AlbumLoader.cs
@@ -35,110 +35,111 @@ using ClutterFlow;
 
 namespace Banshee.ClutterFlow
 {
-	
-	public class BansheeActorLoader<TGen> : ActorLoader<string, TGen> where TGen : ICacheableItem  {
-		bool no_reloading = false;
-		public bool NoReloading {
-			get { return no_reloading; }
-			set { no_reloading = value;	}
-		}
-		
-		private FilterListModel<TGen> model;
-		public virtual FilterListModel<TGen> Model {
-			get { return model; }
-			set {
-				if (value!=model) {
-		            if (model != null) {
-		                model.Cleared -= OnModelClearedHandler;
-		                model.Reloaded -= OnModelReloadedHandler;
-		            }
-					
-					model = value;
-					
-		            if (model != null) {
-		                model.Cleared += OnModelClearedHandler;
-		                model.Reloaded += OnModelReloadedHandler;
-		            }
-		            RefreshCoverManager ();
-				}
-			}
-		}
+    
+    public class BansheeActorLoader<TGen> : ActorLoader<string, TGen> where TGen : ICacheableItem, new()  {
 
-		public BansheeActorLoader (CoverManager coverManager) : base (coverManager) { }
+        private int count; //previous model count
+        
+        private FilterListModel<TGen> model;
+        public virtual FilterListModel<TGen> Model {
+            get { return model; }
+            set {
+                if (value!=model) {
+                    if (model != null) {
+                        model.Cleared -= OnModelClearedHandler;
+                        model.Reloaded -= OnModelReloadedHandler;
+                    }
+                    
+                    model = value;
+                    
+                    if (model != null) {
+                        model.Cleared += OnModelClearedHandler;
+                        model.Reloaded += OnModelReloadedHandler;
+                        count = model.Count;
+                    }
+                    RefreshCoverManager ();
+                }
+            }
+        }
+
+        public BansheeActorLoader (CoverManager coverManager) : base (coverManager) { }
         public override void Dispose ()
         {
             Model = null;
             base.Dispose ();
         }
-		
-		#region Event Handlers		
+        
+        #region Event Handlers        
         protected void OnModelClearedHandler (object o, EventArgs args)
         {
-            Hyena.Log.Information ("OnModelClearedHandler called, no_reloading is " + no_reloading);
-			if (!no_reloading) RefreshCoverManager ();
+            //Hyena.Log.Information ("OnModelClearedHandler called");
+            RefreshCoverManager ();
         }
         
         protected void OnModelReloadedHandler (object o, EventArgs args)
         {
-            Hyena.Log.Information ("OnModelReloadedHandler called, no_reloading is " + no_reloading);
-			if (!no_reloading) RefreshCoverManager ();
+            //Hyena.Log.Information ("OnModelReloadedHandler called");
+            if (count!=model.Count) {
+                count=model.Count;
+                RefreshCoverManager ();
+            }
         }
-		#endregion
-	}
-	
-	public class AlbumLoader : BansheeActorLoader<AlbumInfo>
-	{
+        #endregion
+    }
+    
+    public class AlbumLoader : BansheeActorLoader<AlbumInfo>
+    {
 
-		#region Fields	
-		public AlbumInfo CurrentAlbum {
-			get {
-				if (coverManager.CurrentCover!=null && coverManager.CurrentCover is ClutterFlowAlbum)
-					return (coverManager.CurrentCover as ClutterFlowAlbum).Album;
-				else
-					return null;
-			}
-		}
-		public int CurrentIndex {
-			get {
-				if (coverManager.CurrentCover!=null && coverManager.CurrentCover is ClutterFlowAlbum)
-					return (coverManager.CurrentCover as ClutterFlowAlbum).Index;
-				else
-					return -1;
-			}
-		}
-		#endregion
-		
-		public AlbumLoader (CoverManager coverManager) : base (coverManager) 
-		{
-		}
-		
-		public override List<ClutterFlowActor> GetActors (System.Action<ClutterFlowActor> method_call)
-		{
-			List<ClutterFlowActor> list = new List<ClutterFlowActor>();
-			if (Model!=null) for (int i = 1; i < Model.Count; i++) {
-				ClutterFlowActor actor = AddActorToList(Model[i], list);
-				if (method_call!=null) method_call(actor);
-			}
-			return list;
-		}
-
-		public virtual void ScrollTo (AlbumInfo generator)
-		{
-			coverManager.Timeline.Timeout = 500; //give 'm some time to load the song etc.
-			ScrollTo (ClutterFlowAlbum.CreateCacheKey (generator));
-		}
-		
-		protected override ClutterFlowActor AddActorToList (AlbumInfo generator, List<ClutterFlowActor> list)
-		{
-			string key = ClutterFlowAlbum.CreateCacheKey(generator);
-			ClutterFlowActor actor = Cache.ContainsKey (key) ? Cache[key] : null;
-			if (actor==null) {
-				actor = new ClutterFlowAlbum (generator, coverManager);
-				Cache.Add (key, actor);
-			}
-			actor.Index = list.Count;
-			list.Add(actor);
-			return actor;
-		}
-	}
+        #region Fields    
+        public AlbumInfo CurrentAlbum {
+            get {
+                if (coverManager.CurrentCover!=null && coverManager.CurrentCover is ClutterFlowAlbum)
+                    return (coverManager.CurrentCover as ClutterFlowAlbum).Album;
+                else
+                    return null;
+            }
+        }
+        public int CurrentIndex {
+            get {
+                if (coverManager.CurrentCover!=null && coverManager.CurrentCover is ClutterFlowAlbum)
+                    return (coverManager.CurrentCover as ClutterFlowAlbum).Index;
+                else
+                    return -1;
+            }
+        }
+        #endregion
+        
+        public AlbumLoader (CoverManager coverManager) : base (coverManager) 
+        {
+        }
+        
+        public override List<ClutterFlowActor> GetActors (System.Action<ClutterFlowActor> method_call)
+        {
+            List<ClutterFlowActor> list = new List<ClutterFlowActor>();
+            if (Model!=null) for (int i = 1; i < Model.Count; i++) {
+                ClutterFlowActor actor = AddActorToList(Model[i], list);
+                if (method_call!=null) method_call(actor);
+            }
+            return list;
+        }
+        
+        public virtual void ScrollTo (AlbumInfo generator)
+        {
+            coverManager.Timeline.Timeout = 500; //give 'm some time to load the song etc.
+            ScrollTo (ClutterFlowAlbum.CreateCacheKey (generator));
+        }
+        
+        protected override ClutterFlowActor AddActorToList (AlbumInfo generator, List<ClutterFlowActor> list)
+        {
+            string key = ClutterFlowAlbum.CreateCacheKey(generator);
+            ClutterFlowActor actor = Cache.ContainsKey (key) ? Cache[key] : null;
+            if (actor==null) {
+                actor = new ClutterFlowAlbum (generator, coverManager);
+                Cache.Add (key, actor);
+            }
+            actor.Index = list.Count;
+            list.Add(actor);
+            return actor;
+        }
+    }
 }
