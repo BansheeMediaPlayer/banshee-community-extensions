e11aa14a495dae372d0371a01e1c7818be486767
Gabriel Burt <gabriel.burt@gmail.com>
2010-02-04 14:38:22 -0800

    [build] more build tweaks

--- build/build.environment.mk
+++ build/build.environment.mk
@@ -2,10 +2,9 @@
 MONO_BASE_PATH = 
 MONO_ADDINS_PATH =
 
+# All extensions are libraries
+TARGET = library
+
 # Install Paths
 DEFAULT_INSTALL_DIR = $(pkglibdir)
 EXTENSIONS_INSTALL_DIR = $(DEFAULT_INSTALL_DIR)/Extensions
-
-# Extensions
-REF_EXTENSION_ALARMCLOCK = $(BANSHEE_LIBS)
-
