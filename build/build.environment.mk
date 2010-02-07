0c87667db57aa9c44a1d5dd1b1048f802f319370
Gabriel Burt <gabriel.burt@gmail.com>
2010-02-07 13:27:32 -0800

    [mirage] make && make run work!

--- build/build.environment.mk
+++ build/build.environment.mk
@@ -5,6 +5,8 @@ MONO_ADDINS_PATH =
 # All extensions are libraries
 TARGET = library
 
+DIR_BIN = $(top_builddir)/bin
+
 # Install Paths
 DEFAULT_INSTALL_DIR = $(pkglibdir)
 EXTENSIONS_INSTALL_DIR = $(DEFAULT_INSTALL_DIR)/Extensions
