ee2de19a66a303487a713078606407068d800813
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2010-02-14 16:06:06 +0100

    [Lyrics] Remove duplicate or obsolete source files
    Those are coming from the subversion repository and are probably
    left-overs of a previous source re-organisation.

--- src/Lyrics/Banshee.Lyrics/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("banshee-lyrics-plugin")]
-[assembly: AssemblyDescription("Lyrics plugin for Banshee")]
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
