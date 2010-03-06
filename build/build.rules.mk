7e51326452eb97171afd26ba79c3ee68d0616afe
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2010-03-06 11:16:41 +0100

    [build] Add a common AssemblyInfo file
    This automatically adds the proper version information to all
    assemblies.

--- build/build.rules.mk
+++ build/build.rules.mk
@@ -2,6 +2,7 @@ UNIQUE_FILTER_PIPE = tr [:space:] \\n | sort | uniq
 BUILD_DATA_DIR = $(top_builddir)/bin/share/$(PACKAGE)
 
 SOURCES_BUILD = $(addprefix $(srcdir)/, $(SOURCES))
+SOURCES_BUILD += $(top_srcdir)/src/AssemblyInfo.cs
 
 RESOURCES_EXPANDED = $(addprefix $(srcdir)/, $(RESOURCES))
 RESOURCES_BUILD = $(foreach resource, $(RESOURCES_EXPANDED), \
