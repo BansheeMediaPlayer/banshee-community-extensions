27ec150969f0c858abb1a4b20a43fb1020d3adb4
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2010-11-26 22:21:14 +0100

    [Lirc] Major cleanup
    Remove the empty configuration dialog and the associated actions and
    menu item.
    
    Remove the MockController class, as it doesn't have any real purpose.
    
    Delete an old MonoDevelop solution file.

--- src/Lirc/Banshee.Lirc/LircPlugin.cs
+++ src/Lirc/Banshee.Lirc/LircPlugin.cs
@@ -17,24 +17,10 @@ namespace Banshee.Lirc
         private LircClient lirc;
         private Thread poll;
 		private ActionMapper ctrl;
-		private ActionGroup actions;
-		private InterfaceActionService action_service;
 
         public LircPlugin()
         {
-			actions = new ActionGroup("Lirc");
 			ctrl = new ActionMapper(new BansheeController());
-			actions.Add(new ActionEntry[] {
-				new ActionEntry("LircAction", null, "_Lirc", null,
-				                "Configure the Lirc Addin", null),
-				new ActionEntry("LircConfigureAction", Stock.Properties, "_Configure",
-				                null, "Configure the Lirc addin", OnConfigurePlugin),
-			});
-			
-			action_service = ServiceManager.Get<InterfaceActionService>();
-			
-			action_service.UIManager.InsertActionGroup(actions, 0);
-            action_service.UIManager.AddUiFromResource("Ui.xml");
         }
 
 		void IExtensionService.Initialize()
@@ -50,17 +36,6 @@ namespace Banshee.Lirc
             lirc.Dispose ();
             lirc = null;
 		}
-
-		public void OnConfigurePlugin(object o, EventArgs args)
-		{
-            ConfigDialog dlg = new ConfigDialog();
-			dlg.Run();
-			dlg.Destroy();
-		}
-
-		~LircPlugin()
-        {
-        }
 
         private void PollThread()
         {
