d201f453fd26dfba3e6fd7d06658877faa5300f6
Frank Ziegler <funtastix@googlemail.com>
2011-01-26 11:57:18 +0700

    [LiveRadio] Allow only single selection in plugin list

--- src/LiveRadio/Banshee.LiveRadio/LiveRadioPlugins/LiveRadioPluginListModel.cs
+++ src/LiveRadio/Banshee.LiveRadio/LiveRadioPlugins/LiveRadioPluginListModel.cs
@@ -1,3 +1,28 @@
+//
+// LiveRadioPluginListModel.cs
+//
+// Author:
+//   Frank Ziegler <funtastix@googlemail.com>
+//
+// Copyright (c) 2010 Frank Ziegler
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 using System;
 using System.Collections.Generic;
@@ -21,13 +46,13 @@ namespace Banshee.LiveRadio.Plugins
         private ILiveRadioPlugin[] list;
         private List<ILiveRadioPlugin> original_list;
 
-        private Selection selection;
+        private SingleSelection selection;
 
         public LiveRadioPluginListModel (List<ILiveRadioPlugin> list) : base()
         {
             original_list = list;
             this.list = original_list.ToArray ();
-            selection = new Selection ();
+            selection = new SingleSelection ();
         }
 
         public void SetList (List<ILiveRadioPlugin> newlist)
@@ -98,7 +123,7 @@ namespace Banshee.LiveRadio.Plugins
 
         public void SetSelection (Selection selection)
         {
-            this.selection = selection;
+            this.selection = (SingleSelection)selection;
         }
 
         public Selection Selection {
