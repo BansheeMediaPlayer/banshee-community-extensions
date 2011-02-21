8fb4c398a3e4c12645f8e723f02f55bfb34156f2
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2011-02-21 22:56:44 +0100

    Karaoke: Improve layout of the configuration dialog
    Request a specific width and height for the dialog and use a table to
    layout the scales and their labels.
    Remove artificial line breaks in the labels.

--- src/Karaoke/Banshee.Karaoke/KaraokeConfigDialog.cs
+++ src/Karaoke/Banshee.Karaoke/KaraokeConfigDialog.cs
@@ -29,10 +29,12 @@ using System;
 using Gtk;
 using Mono.Addins;
 
+using Banshee.Gui.Dialogs;
+
 namespace Banshee.Karaoke
 {
 
-    public class KaraokeConfigDialog : Dialog
+    public class KaraokeConfigDialog : BansheeDialog
     {
         KaraokeService karaoke_service;
 
@@ -71,40 +73,40 @@ namespace Banshee.Karaoke
             preferences_image.Yalign = 0f;
             preferences_image.IconName = "gtk-preferences";
             preferences_image.IconSize = (int)IconSize.Dialog;
-            preferences_image.Show ();
 
-            header_label.Text = String.Format (AddinManager.CurrentLocalizer.GetString ("{0}Karaoke configuration\n{1}"), "<span weight=\"bold\" size=\"larger\">", "</span>");
+            header_label.Text = String.Format ("<span weight=\"bold\" size=\"larger\">{0}</span>", AddinManager.CurrentLocalizer.GetString ("Karaoke configuration"));
             header_label.UseMarkup = true;
-            header_label.Wrap = true;
             header_label.Yalign = 0f;
             header_label.Xalign = 0f;
-            description_label.Text = AddinManager.CurrentLocalizer.GetString ("You can alter the parameters of the karaoke effect here  and\n"
-                                                                              + "define custom filter-band, filter-width and effect level.\n");
+            description_label.Text = AddinManager.CurrentLocalizer.GetString ("You can alter the parameters of the karaoke effect here and "
+                                                                              + "define custom filter-band, filter-width and effect level.");
             description_label.Yalign = 0f;
             description_label.Xalign = 0f;
+            description_label.Wrap = true;
 
             choose_level_label.Text = AddinManager.CurrentLocalizer.GetString ("Effect Level:");
             choose_band_label.Text = AddinManager.CurrentLocalizer.GetString ("Filter Band:");
             choose_width_label.Text = AddinManager.CurrentLocalizer.GetString ("Filter Width:");
+            choose_level_label.Xalign = choose_band_label.Xalign = choose_width_label.Xalign = 0f;
 
             level_scale.Digits = 0;
             level_scale.DrawValue = true;
-            level_scale.TooltipText = AddinManager.CurrentLocalizer.GetString ("The effect level defines to what degree the voice\n"
-                                                                              + "frequencies are reduced.\n");
+            level_scale.TooltipText = AddinManager.CurrentLocalizer.GetString ("The effect level defines to what degree the voice "
+                                                                              + "frequencies are reduced.");
             level_scale.ValuePos = PositionType.Right;
             level_scale.Value = current_level * 100;
             level_scale.ValueChanged += OnLevelValueChanged;
             band_scale.Digits = 0;
             band_scale.DrawValue = true;
-            band_scale.TooltipText = AddinManager.CurrentLocalizer.GetString ("The filter band defines position of filter within\n"
-                                                                              + "the frequency spectrum.\n");
+            band_scale.TooltipText = AddinManager.CurrentLocalizer.GetString ("The filter band defines position of filter within "
+                                                                              + "the frequency spectrum.");
             band_scale.ValuePos = PositionType.Right;
             band_scale.Value = current_band;
             band_scale.ValueChanged += OnBandValueChanged;
             width_scale.Digits = 0;
             width_scale.DrawValue = true;
-            width_scale.TooltipText = AddinManager.CurrentLocalizer.GetString ("The filter width defines how wide the spectrum\n"
-                                                                              + "of the filtered frequencies is.\n");
+            width_scale.TooltipText = AddinManager.CurrentLocalizer.GetString ("The filter width defines how wide the spectrum "
+                                                                              + "of the filtered frequencies is.");
             width_scale.ValuePos = PositionType.Right;
             width_scale.Value = current_width;
             width_scale.ValueChanged += OnWidthValueChanged;
@@ -120,27 +122,28 @@ namespace Banshee.Karaoke
             HBox main_container = new HBox ();
             VBox action_container = new VBox ();
 
-            main_container.Spacing = 12;
-            main_container.BorderWidth = 6;
-
             action_container.PackStart (header_label, true, true, 0);
             action_container.PackStart (description_label, true, true, 0);
-            VBox choosing_labels = new VBox ();
-            choosing_labels.PackStart (choose_level_label, false, false, 5);
-            choosing_labels.PackStart (choose_band_label, false, false, 5);
-            choosing_labels.PackStart (choose_width_label, false, false, 5);
-            VBox box_choosing = new VBox ();
-            box_choosing.PackStart (level_scale, true, true, 0);
-            box_choosing.PackStart (band_scale, true, true, 0);
-            box_choosing.PackStart (width_scale, true, true, 0);
-            HBox all_choosing = new HBox ();
-            all_choosing.Spacing = 10;
-            all_choosing.PackStart (choosing_labels, false, false, 0);
-            all_choosing.PackStart (box_choosing, true, true, 0);
 
-            action_container.PackStart (all_choosing, true, true, 5);
+            Table table = new Table (3, 2, false);
+            table.RowSpacing = 6;
+            table.ColumnSpacing = 12;
+            table.Attach (choose_level_label, 0, 1, 0, 1,
+                          AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+            table.Attach (level_scale, 1, 2, 0, 1,
+                          AttachOptions.Expand | AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+            table.Attach (choose_band_label, 0, 1, 1, 2,
+                          AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+            table.Attach (band_scale, 1, 2, 1, 2,
+                          AttachOptions.Expand | AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+            table.Attach (choose_width_label, 0, 1, 2, 3,
+                          AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+            table.Attach (width_scale, 1, 2, 2, 3,
+                          AttachOptions.Expand | AttachOptions.Fill, AttachOptions.Fill, 0, 0);
 
-            main_container.PackStart (preferences_image, true, true, 5);
+            action_container.PackStart (table, true, true, 5);
+
+            main_container.PackStart (preferences_image, false, true, 5);
             main_container.PackEnd (action_container, true, true, 5);
             this.VBox.PackStart (main_container, true, true, 5);
 
@@ -155,13 +158,10 @@ namespace Banshee.Karaoke
 
             Title = "Karaoke configuration";
             IconName = "gtk-preferences";
-            Resizable = false;
-            BorderWidth = 6;
-            HasSeparator = false;
-            this.VBox.Spacing = 12;
+            HeightRequest = 260;
+            WidthRequest = 500;
 
             ShowAll ();
-
         }
 
         void OnLevelValueChanged (object sender, EventArgs e)
