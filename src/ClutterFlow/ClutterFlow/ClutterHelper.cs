e84ea8b159c8d60db3bffa0036d9535e56328fff
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2011-05-03 22:11:34 +0200

    ClutterFlow: Remove some commented-out code

--- src/ClutterFlow/ClutterFlow/ClutterHelper.cs
+++ src/ClutterFlow/ClutterFlow/ClutterHelper.cs
@@ -52,24 +52,6 @@ namespace ClutterFlow
 		[DllImport ("libclutter-glx-1.0.so.0")]
 		private static extern void clutter_actor_box_from_vertices (ref ActorBox box, IntPtr[] vtx);
 		
-		/*[DllImport("libGL.so", EntryPoint = "glGetString")]
-		private static extern IntPtr glGetString(int s);
-
-		public static string OpenGLGetString (int i)
-		{
-			return Marshal.PtrToStringAnsi (glGetString (i));
-		}
-		public static bool CheckForExtension (string extension)
-		{
-			string extensions = OpenGLGetString(Cogl.GL.GL_EXTENSIONS);
-			if (String.IsNullOrEmpty (extensions)) {
-				return false;
-			}
-            else {
-				return extensions.Contains (extension);
-			}
-		}*/
-		
 		
 		public unsafe static ActorBox GetAbsAllocationBox (Actor actor)
 		{
