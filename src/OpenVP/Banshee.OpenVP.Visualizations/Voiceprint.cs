2029878ebfd6a64a717e1e5d0381502ba43123e5
Nicholas Parker <nickbp@gmail.com>
2011-08-17 21:05:05 +0200

    OpenVP: Add a new "Scaled Voiceprint" visualization
    Signed-off-by: Bertrand Lorentz <bertrand.lorentz@gmail.com>

--- src/OpenVP/Banshee.OpenVP.Visualizations/Voiceprint.cs
+++ src/OpenVP/Banshee.OpenVP.Visualizations/Voiceprint.cs
@@ -3,8 +3,10 @@
 //
 // Author:
 //       Chris Howie <cdhowie@gmail.com>
+//       Nicholas Parker <nickbp@gmail.com>
 //
 // Copyright (c) 2009 Chris Howie
+// Copyright (c) 2011 Nicholas Parker
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -111,6 +113,128 @@ namespace Banshee.OpenVP.Visualizations
             Gl.glBegin(Gl.GL_POINTS);
             for (int i = 0; i < spectrumBuffer.Length; i++) {
                 float v = spectrumBuffer[i];
+
+                Color.FromHSL(120 * (1 - v), 1, Math.Min(0.5f, v)).Use();
+                //Gl.glColor3f(v, v, v);
+
+                Gl.glVertex2i(controller.Width - 1, i);
+            }
+            Gl.glEnd();
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glPopMatrix();
+
+            Gl.glMatrixMode(Gl.GL_TEXTURE);
+            Gl.glPopMatrix();
+
+            Gl.glPopAttrib();
+        }
+
+        public void Dispose()
+        {
+            if (buffer != null) {
+                buffer.Dispose();
+                buffer = null;
+            }
+        }
+    }
+
+    public class ScaleVoiceprint : IRenderer, IDisposable
+    {
+        //cached mapping: for each index in controller.Height, what index to retrieve from spectrumBuffer
+        private const double scale = 1.25;
+        private int[] spectrumScaling;
+
+        private float[] spectrumBuffer;
+
+        private TextureHandle buffer;
+
+        public ScaleVoiceprint()
+        {
+        }
+
+        private void EnsureBufferLengths(int dataLength, int viewLength)
+        {
+            bool rescale = false;
+            if (spectrumBuffer == null || spectrumBuffer.Length != dataLength) {
+                spectrumBuffer = new float[dataLength];
+                rescale = true;
+            }
+            if (spectrumScaling == null || spectrumScaling.Length != viewLength) {
+                spectrumScaling = new int[viewLength];
+                rescale = true;
+            }
+            if (rescale) {
+                //formula: dataLength * viewLength^scale / i^scale
+                double multiplier = dataLength / Math.Pow(viewLength, scale);
+                for (int i = 0; i < viewLength; ++i) {
+                    spectrumScaling[i] = (int)(Math.Pow(i, scale) * multiplier);
+                }
+            }
+        }
+
+        private void ResizeTexture(int w, int h)
+        {
+            if (buffer == null) {
+                buffer = new TextureHandle(w, h);
+            } else {
+                buffer.SetTextureSize(w, h);
+            }
+        }
+
+        public void Render(IController controller)
+        {
+            EnsureBufferLengths(controller.PlayerData.NativeSpectrumLength, controller.Height);
+
+            controller.PlayerData.GetSpectrum(spectrumBuffer);
+
+            Gl.glPushAttrib(Gl.GL_ENABLE_BIT);
+            Gl.glDisable(Gl.GL_DEPTH_TEST);
+            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_DECAL);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_CLAMP);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_CLAMP);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glPushMatrix();
+            Gl.glLoadIdentity();
+
+            Glu.gluOrtho2D(0, controller.Width, 0, controller.Height);
+
+            Gl.glMatrixMode(Gl.GL_TEXTURE);
+            Gl.glPushMatrix();
+            Gl.glLoadIdentity();
+
+            Gl.glScalef(1f / controller.Width, 1f / controller.Height, 1f);
+
+            ResizeTexture(controller.Width, controller.Height);
+
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, buffer.TextureId);
+            Gl.glCopyTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, 0, 0,
+                                controller.Width, controller.Height, 0);
+
+            Gl.glColor4f(1, 1, 1, 1);
+
+            Gl.glEnable(Gl.GL_TEXTURE_2D);
+            Gl.glBegin(Gl.GL_QUADS);
+
+            Gl.glTexCoord2f(1, 0);
+            Gl.glVertex2f(0, 0);
+
+            Gl.glTexCoord2f(1, controller.Height);
+            Gl.glVertex2f(0, controller.Height);
+
+            Gl.glTexCoord2f(controller.Width, controller.Height);
+            Gl.glVertex2f(controller.Width - 1, controller.Height);
+
+            Gl.glTexCoord2f(controller.Width, 0);
+            Gl.glVertex2f(controller.Width - 1, 0);
+
+            Gl.glEnd();
+            Gl.glDisable(Gl.GL_TEXTURE_2D);
+
+            Gl.glBegin(Gl.GL_POINTS);
+            for (int i = 0; i < controller.Height; i++) {
+                float v = spectrumBuffer[spectrumScaling[i]];
 
                 Color.FromHSL(120 * (1 - v), 1, Math.Min(0.5f, v)).Use();
                 //Gl.glColor3f(v, v, v);
