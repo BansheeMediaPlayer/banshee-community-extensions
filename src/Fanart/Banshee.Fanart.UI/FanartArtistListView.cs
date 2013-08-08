5f3faba858f20db6af81556a1a5a0e1436cd15c5
Tomasz Maczynski <tmtimon@gmail.com>
2013-08-08 23:14:44 +0200

    Fanart: add FanartArtistListView.cs

---
+++ src/Fanart/Banshee.Fanart.UI/FanartArtistListView.cs
@@ -0,0 +1,52 @@
+//
+// FanartArtistListView.cs
+//
+// Author:
+//   Tomasz Maczyński <tmtimon@gmail.com>
+//
+// Copyright 2013 Tomasz Maczyński
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using Banshee.Collection.Gui;
+using Banshee.Collection;
+using Hyena.Data.Gui;
+
+namespace Banshee.Fanart.UI
+{
+    public class FanartArtistListView : TrackFilterListView<ArtistInfo>
+    {
+        protected FanartArtistListView (IntPtr ptr) : base () {}
+
+        public FanartArtistListView () : base ()
+        {
+            //column_controller.Add (new Column ("Artist", new ColumnCellText ("DisplayName", true), 1.0));
+            column_controller.Add (new Column ("Artist", new ColumnCellText ("DisplayName", true), 0.5));
+            column_controller.Add (new Column ("Name", new ColumnCellText ("Name", true), 0.5));
+            ColumnController = column_controller;
+        }
+       
+        protected override void OnModelReloaded ()
+        {
+            Hyena.Log.Debug ("OnModelReloaded in FanartArtistListView");
+            base.OnModelReloaded ();
+        }
+    }
+}
+
