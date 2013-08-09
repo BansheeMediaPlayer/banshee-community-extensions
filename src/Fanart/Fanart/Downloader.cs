05dcfc1054e53cb7a6ec1f3eb99d4d0c0dc7a05b
Tomasz Maczynski <tmtimon@gmail.com>
2013-08-09 16:05:24 +0200

    Fanart: add Downloader class
    Add static Downloader class with Download () method.

---
+++ src/Fanart/Fanart/Downloader.cs
@@ -0,0 +1,48 @@
+//
+// Downloader.cs
+//
+// Author:
+//   Tomasz Maczyński <tmtimon@gmail.com>
+//
+// Copyright 2013 Tomasz Maczyński
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Net;
+
+namespace Fanart
+{
+    public static class Downloader
+    {
+        public static string Download(string uri)
+        {
+            if (String.IsNullOrEmpty (uri)) {
+                throw new ArgumentException("Specify uri of resource you want to download");
+            }
+
+            string response;
+            using (WebClient client = new WebClient ()) {
+                response = client.DownloadString (uri);
+            }
+
+            return response;
+        }
+    }
+}
+
