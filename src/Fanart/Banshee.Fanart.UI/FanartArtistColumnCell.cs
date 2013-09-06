083b1dbd8849179d1cb5112926c383c2b6c9e614
Tomasz Maczynski <tmtimon@gmail.com>
2013-09-06 21:45:13 +0200

    Fanart: FanartArtistColumnCell inherits from ColumnCell

--- src/Fanart/Banshee.Fanart.UI/FanartArtistColumnCell.cs
+++ src/Fanart/Banshee.Fanart.UI/FanartArtistColumnCell.cs
@@ -24,12 +24,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using Hyena.Data.Gui;
 
 namespace Banshee.Fanart.UI
 {
-    public class FanartArtistColumnCell
+    public class FanartArtistColumnCell : ColumnCell
     {
-        public FanartArtistColumnCell ()
+        public FanartArtistColumnCell () : base (null, true)
         {
         }
     }
