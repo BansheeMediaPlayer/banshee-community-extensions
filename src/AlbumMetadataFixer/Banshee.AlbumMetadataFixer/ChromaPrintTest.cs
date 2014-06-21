cf9bae3620a63d108c51bb9ef9b98f861e79a221
Marcin Kolny <marcin.kolny@gmail.com>
2014-06-21 18:22:58 +0200

    AlbumMetadataFixer: ChromaPrint test refactoring
    Extracted code responsible for xml reading to another class.

--- src/AlbumMetadataFixer/Banshee.AlbumMetadataFixer/ChromaPrintTest.cs
+++ src/AlbumMetadataFixer/Banshee.AlbumMetadataFixer/ChromaPrintTest.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Xml;
 using Gst;
 
 namespace AlbumMetadataFixer
 {
+	#region Helper classes
 	class ReleaseGroup
 	{
 		public string ID
@@ -36,242 +38,251 @@ namespace AlbumMetadataFixer
 		}
 	}
 
-    class Recording
-    {
-        public string ID
-        { private set; get; }
+	class Recording
+	{
+		public string ID
+		{ private set; get; }
 
-        public string Title
-        { private set; get; }
+		public string Title
+		{ private set; get; }
 
-        public List<Artist> Artists
-        { private set; get; }
+		public List<Artist> Artists
+		{ private set; get; }
 
-        public List<ReleaseGroup> ReleaseGroups
-        { private set; get; }
+		public List<ReleaseGroup> ReleaseGroups
+		{ private set; get; }
 
 		public Recording(string id, string title, List<Artist> artists, List<ReleaseGroup> release_groups) {
-            ID = id;
-            Title = title;
-            Artists = artists;
-            ReleaseGroups = release_groups;
-        }
-    }
+			ID = id;
+			Title = title;
+			Artists = artists;
+			ReleaseGroups = release_groups;
+		}
+	}
+	#endregion Helper classes
 
-    class AcoustIDReader
-    {
-        string fingerprint = null;
-        long duration = -1;
-        string filename;
-        string current_id;
-        string key;
-        Pipeline pipeline;
-        private List<Recording> recordings;
+	class AcoustIDReader
+	{
+		private string fingerprint;
+		private long duration = -1;
+		private string filename;
+		private string key;
+		private Pipeline pipeline;
+		private Action<string, List<Recording>> completionHandler;
 
-        Action<string, List<Recording>> completion_handler;
+		public AcoustIDReader (string key) {
+			this.key = key;
+		}
 
-        public AcoustIDReader (string key) {
-            this.key = key;
-        }
+		public void GetID (string filename, Action<string, List<Recording>> completion_handler) {
+			this.filename = filename;
+			completionHandler = completion_handler;
+			StartPipeline ();
+		}
 
-        public void GetID (string filename, Action<string, List<Recording>> completion_handler) {
-            this.filename = filename;
-            this.completion_handler = completion_handler;
-            StartPipeline ();
-        }
+		private void StartPipeline () {
+			pipeline = new Pipeline ();
 
-        public void StartPipeline () {
-            pipeline = new Pipeline ();
+			Element src = ElementFactory.Make ("filesrc", "source"),
+			decoder = ElementFactory.Make ("decodebin", "decoder"),
+			chroma_print = ElementFactory.Make ("chromaprint", "processor"),
+			sink = ElementFactory.Make ("fakesink", "sink");
 
-            Element src = ElementFactory.Make ("filesrc", "source"),
-            decoder = ElementFactory.Make ("decodebin", "decoder"),
-            chroma_print = ElementFactory.Make ("chromaprint", "processor"),
-            sink = ElementFactory.Make ("fakesink", "sink");
+			pipeline.Bus.AddSignalWatch();
+			pipeline.Bus.Message += MsgHandler;
 
-            pipeline.Bus.AddSignalWatch();
-            pipeline.Bus.Message += MsgHandler;
+			if (src == null || decoder == null || chroma_print == null || sink == null) {
+				Console.WriteLine ("cannot find necessary gstreamer element (filesrc, decodebin, chromaprint or fakesink)!");
+				pipeline = null;
+				return;
+			}
 
-            if (src == null || decoder == null || chroma_print == null || sink == null) {
-                Console.WriteLine ("cannot find necessary gstreamer element (filesrc, decodebin, chromaprint or fakesink)!");
-                pipeline = null;
-                return;
-            }
+			sink ["sync"] = 0;
+			src ["location"] = filename;
 
-            sink ["sync"] = 0;
-            src ["location"] = filename;
+			pipeline.Add (src);
+			pipeline.Add (decoder);
+			pipeline.Add (chroma_print);
+			pipeline.Add (sink);
 
-            pipeline.Add (src);
-            pipeline.Add (decoder);
-            pipeline.Add (chroma_print);
-            pipeline.Add (sink);
+			src.Link (decoder);
+			chroma_print.Link (sink);
 
-            src.Link (decoder);
-            chroma_print.Link (sink);
+			decoder.PadAdded += (o, args) => {
+				args.NewPad.Link (chroma_print.GetStaticPad ("sink"));
+			};
 
-            decoder.PadAdded += (o, args) => {
-                args.NewPad.Link (chroma_print.GetStaticPad ("sink"));
-            };
+			pipeline.SetState (State.Playing);
+		}
 
-            pipeline.SetState (State.Playing);
-        }
+		private void MsgHandler(object o, MessageArgs args) {
+			switch (args.Message.Type) {
+				case MessageType.DurationChanged:
+				if (pipeline.QueryDuration (Format.Time, out duration)) {
+					duration /= Gst.Constants.SECOND;
+					ReadID ();
+				}
+				break;
+				case MessageType.Eos:
+				// todo: finish
+				break;
+				case MessageType.Tag:
+				TagList tags = args.Message.ParseTag();
+				tags.GetString("chromaprint-fingerprint", out fingerprint);
 
-        private void ReadID () {
-            current_id = string.Empty;
+				if (fingerprint != null) {
+					ReadID ();
+				}
+				break;
+			}
+		}
 
-            if (fingerprint == null || duration == -1) {
-                // todo: timeout or sth?
-                return;
-            }
+		private void ReadID () {
 
-            string url = string.Format ("http://api.acoustid.org/v2/lookup?meta=recordings+releasegroups&format=xml&client={0}&duration={1}&fingerprint={2}", key, duration, fingerprint);
-                                                
-            var reader = new System.Xml.XmlTextReader (url);
-            //var reader = new System.Xml.XmlTextReader ("/home/loganek/lookup");
-            var doc = new System.Xml.XmlDocument ();
-            doc.Load (reader);
+			if (fingerprint == null || duration == -1) {
+				// todo: timeout or sth?
+				return;
+			}
 
-            System.Xml.XmlNode status = doc.SelectSingleNode ("/response/status");
+			string url = string.Format ("http://api.acoustid.org/v2/lookup?meta=recordings+releasegroups&format=xml&client={0}&duration={1}&fingerprint={2}", key, duration, fingerprint);
+			var xmlReader = new XmlAcoustIDReader (url, completionHandler);
+			xmlReader.ReadID ();
+		}
+	}
 
-            if (status == null) {
-                OnCompleted ();
-            }
+	class XmlAcoustIDReader
+	{
+		private string current_id;
+		private string url;
+		private List<Recording> recordings;
+		private Action<string, List<Recording>> completionHandler;
 
-            string response_status = status.InnerText;
+		public XmlAcoustIDReader (string url, Action<string, List<Recording>> completion_handler) {
+			this.url = url;
+			this.completionHandler = completion_handler;
+		}
 
-            if (response_status != "ok") {
-                OnCompleted ();
-            }
+		public void ReadID () {
+			var reader = new XmlTextReader (url);
+			var doc = new XmlDocument ();
 
-            FindBestID (doc.SelectNodes ("/response/results/result"));
-            OnCompleted ();
-        }
+			current_id = string.Empty;
+			doc.Load (reader);
+			XmlNode status = doc.SelectSingleNode ("/response/status");
 
-        private void FindBestID (System.Xml.XmlNodeList results) {
-            double current_score = 0;
+			if (status == null) {
+				OnCompleted ();
+			}
 
-            foreach (System.Xml.XmlNode result in results) {
-                double score;
+			if (status.InnerText != "ok") {
+				OnCompleted ();
+			}
 
-                if (result ["score"] == null || result ["id"] == null || !double.TryParse (result ["score"].InnerText, out score)) {
-                    continue;
-                }
+			FindBestID (doc.SelectNodes ("/response/results/result"));
+			OnCompleted ();
+		}
 
-                if (score > current_score) {
-                    current_score = score;
-                    current_id = result ["id"].InnerText;
+		private void FindBestID (XmlNodeList results) {
+			double current_score = 0;
+			foreach (XmlNode result in results) {
+				double score;
 
-                    ProcessRecordings (result);
-                }
-            }
-        }
+				if (result ["score"] == null || result ["id"] == null || !double.TryParse (result ["score"].InnerText, out score)) {
+					continue;
+				}
 
-        private void ProcessRecordings (System.Xml.XmlNode result) {
-            recordings = new List<Recording>();
-            foreach (System.Xml.XmlNode recording in result.SelectNodes ("recordings/recording")) {
-                if (recording ["id"] != null) {
-                    recordings.Add (new Recording (
+				if (score > current_score) {
+					current_score = score;
+					current_id = result ["id"].InnerText;
+					ReadRecordings (result);
+				}
+			}
+		}
+
+		private void ReadRecordings (XmlNode result) {
+			recordings = new List<Recording>();
+			foreach (XmlNode recording in result.SelectNodes ("recordings/recording")) {
+				if (recording ["id"] != null) {
+					recordings.Add (new Recording (
 						recording ["id"].InnerText, 
 						GetInner (recording ["title"]), 
 						ReadArtists (recording), 
 						ReadReleaseGroups (recording)
 						));
-                }
-            }
-        }
+				}
+			}
+		}
 
-        private List<Artist> ReadArtists (System.Xml.XmlNode result) {
-			var list = new List<Artist> ();
-            foreach (System.Xml.XmlNode artist in result.SelectNodes ("artists/artist")) {
-                if (artist ["name"] != null) {
-					list.Add (new Artist (artist ["id"].InnerText, GetInner (artist ["name"])));
-                }
-            }
-            return list;
-        }
+		private void OnCompleted ()
+		{
+			if (completionHandler != null) {
+				completionHandler (current_id, recordings);
+			}
+		}
 
-        private List<ReleaseGroup> ReadReleaseGroups (System.Xml.XmlNode result) {
+		private static List<ReleaseGroup> ReadReleaseGroups (XmlNode result)
+		{
 			var list = new List<ReleaseGroup> ();
-            foreach (System.Xml.XmlNode releasegroup in result.SelectNodes ("releasegroups/releasegroup")) {
-                if (releasegroup ["id"] != null) {
-					list.Add (new ReleaseGroup(
-						releasegroup ["id"].InnerText, 
-						GetInner (releasegroup ["title"]), 
-						GetInner (releasegroup ["type"])
-						));
-                }
-            }
-            return list;
-        }
+			foreach (XmlNode releasegroup in result.SelectNodes ("releasegroups/releasegroup")) {
+				if (releasegroup ["id"] != null) {
+					list.Add (new ReleaseGroup (releasegroup ["id"].InnerText, GetInner (releasegroup ["title"]), GetInner (releasegroup ["type"])));
+				}
+			}
+			return list;
+		}
 
-        private void MsgHandler(object o, MessageArgs args) {
-            switch (args.Message.Type) {
-                case MessageType.DurationChanged:
-                if (pipeline.QueryDuration (Format.Time, out duration)) {
-                    duration /= Gst.Constants.SECOND;
-                    ReadID ();
-                }
-                break;
-                case MessageType.Eos:
-                // todo: finish
-                break;
-                case MessageType.Tag:
-                TagList tags = args.Message.ParseTag();
-                tags.GetString("chromaprint-fingerprint", out fingerprint);
+		private static List<Artist> ReadArtists (XmlNode result)
+		{
+			var list = new List<Artist> ();
+			foreach (XmlNode artist in result.SelectNodes ("artists/artist")) {
+				if (artist ["name"] != null) {
+					list.Add (new Artist (artist ["id"].InnerText, GetInner (artist ["name"])));
+				}
+			}
+			return list;
+		}
 
-                if (fingerprint != null) {
-                    ReadID ();
-                }
-                break;
-            }
-        }
-
-        private void OnCompleted ()
-        {
-            if (completion_handler != null) {
-                completion_handler (current_id, recordings);
-            }
-        }
-
-		private static string GetInner(System.Xml.XmlNode node) {
+		private static string GetInner(XmlNode node) {
 			return node == null ? "" : node.InnerText;
 		}
-    }
 
-    class ChromaPrintTest
-    {
-        static GLib.MainLoop Loop;
+	}
 
-        public static void Main(string[] argv)
-        {
-            if (argv.Length < 1) {
-                Console.WriteLine ("Usage: ChromaPrintTest.exe <audio file>");
-                return;
-            }
+	class ChromaPrintTest
+	{
+		static GLib.MainLoop Loop;
 
-            Application.Init ();
-            Loop = new GLib.MainLoop();
+		public static void Main(string[] argv)
+		{
+			if (argv.Length < 1) {
+				Console.WriteLine ("Usage: ChromaPrintTest.exe <audio file>");
+				return;
+			}
 
-            var reader = new AcoustIDReader ("8XaBELgH"); // todo: it's example key. Banshee should be registered in acoustID system.
-            reader.GetID (argv [0], (id, list) => {
-                Console.WriteLine ("Track ID: " + id);
-                foreach (Recording rec in list) {
-                    Console.WriteLine ("=========================");
-                    Console.WriteLine ("Recording ID: " + rec.ID);
-                    Console.WriteLine ("Title: " + rec.Title);
-                    Console.WriteLine ("Artists: ");
-                    foreach (Artist artist in rec.Artists) {
+			Application.Init ();
+			Loop = new GLib.MainLoop();
+
+			var reader = new AcoustIDReader ("8XaBELgH"); // todo: it's example key. Banshee should be registered in acoustID system.
+			reader.GetID (argv [0], (id, list) => {
+				Console.WriteLine ("Track ID: " + id);
+				foreach (Recording rec in list) {
+					Console.WriteLine ("=========================");
+					Console.WriteLine ("Recording ID: " + rec.ID);
+					Console.WriteLine ("Title: " + rec.Title);
+					Console.WriteLine ("Artists: ");
+					foreach (Artist artist in rec.Artists) {
 						Console.WriteLine ("\t * {0} (ID: {1})", artist.Name, artist.ID);
-                    }
-                    Console.WriteLine("Release Groups: ");
-                    foreach (ReleaseGroup release_group in rec.ReleaseGroups) {
+					}
+					Console.WriteLine("Release Groups: ");
+					foreach (ReleaseGroup release_group in rec.ReleaseGroups) {
 						Console.WriteLine ("\t * {0} (Type: {1}, ID: {2})", release_group.Title, release_group.Type, release_group.ID);
-                    }
-                }
+					}
+				}
 
-                Loop.Quit ();
-            });
+				Loop.Quit ();
+			});
 
-            Loop.Run();
-        }
-    }
+			Loop.Run();
+		}
+	}
 }
