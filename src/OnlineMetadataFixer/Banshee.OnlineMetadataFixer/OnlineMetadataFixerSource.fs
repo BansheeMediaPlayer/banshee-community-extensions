72791993796c5f5b23abdb80e7d8e89ebadd281d
Marcin Kolny <marcin.kolny@gmail.com>
2014-06-27 15:47:03 +0200

    OnlineMetadataFixer: restored previous version of a OnlineMetadataFixer

--- src/OnlineMetadataFixer/Banshee.OnlineMetadataFixer/OnlineMetadataFixerSource.fs
+++ src/OnlineMetadataFixer/Banshee.OnlineMetadataFixer/OnlineMetadataFixerSource.fs
@@ -28,19 +28,50 @@ namespace Banshee.OnlineMetadataFixer
 
 open System;
 open System.Linq;
+open System.Xml;
 open System.Collections.Generic;
-
 open Mono.Unix;
+open Hyena.Data.Sqlite;
+open Banshee.ServiceStack;
 
 type OnlineMetadataFixerSource () = 
     inherit Banshee.Fixup.Solver()
-    do
-        base.Id <- "online-fix";
-        base.Name <- Catalog.GetString ("Online Fix");
-        base.Description <- Catalog.GetString ("Displayed are tracks loaded in Banshee");
+        do
+            base.Id <- "empty-albums";
+            base.Name <- Catalog.GetString ("Empty Album Name");
+            base.Description <- Catalog.GetString ("Displayed are tracks with empty album's name.");       
 
+        let find_cmd = new HyenaSqliteCommand (String.Format (@"
+            INSERT INTO MetadataProblems (ProblemType, TypeOrder, Generation, SolutionOptions, ObjectIds)
+                SELECT
+                    'empty-albums', 1, ?,
+                     Title || ',' || IFNULL((SELECT Name from CoreArtists where ArtistID = CoreTracks.ArtistID), ''),
+                    AlbumID || ',' || TrackID
+                FROM CoreTracks
+                WHERE IFNULL((SELECT Title from CoreAlbums where AlbumID = CoreTracks.AlbumID), '') = ''
+                    GROUP BY TrackID 
+                    ORDER BY Title"));
+
+    static member GetAlbumTitle (artist : String, title : String) : String =
+        let url = String.Format("http://musicbrainz.org/ws/1/track/?type=xml&artist={0}&title={1}", artist, title);
+        let mutable album_name = Unchecked.defaultof<String>;
+        let reader = new XmlTextReader (url);
+        while reader.Read() && album_name = Unchecked.defaultof<String> do 
+            if reader.Name = "release" then
+                reader.Read () |> ignore;
+                while reader.Name <> "release" && album_name = Unchecked.defaultof<String> do
+                    if reader.Name = "title" then
+                        album_name <- reader.ReadString ();
+            reader.Read();
+        album_name;                
+    
     override this.IdentifyCore () =              
-        ()
+        ServiceManager.DbConnection.Execute ("DELETE FROM CoreAlbums WHERE AlbumID NOT IN (SELECT DISTINCT(AlbumID) FROM CoreTracks)") |> ignore;
+        ServiceManager.DbConnection.Execute (find_cmd, this.Generation) |> ignore;
 
     override this.Fix (problems) =
-        ()
+        for problem in problems do
+            let track_info = problem.SolutionOptions. [0].Split(',');
+            ServiceManager.DbConnection.Execute (@"UPDATE CoreAlbums SET Title = ? WHERE AlbumID = ?;",
+               OnlineMetadataFixerSource.GetAlbumTitle(track_info. [1], track_info. [0]), problem.ObjectIds. [0]) |> ignore;
+            
