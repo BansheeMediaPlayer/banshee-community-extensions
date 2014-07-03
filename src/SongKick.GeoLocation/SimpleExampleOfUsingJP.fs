9c4ff57fb57e8e7ab09a19db67612df3aaadb0f6
Dmitrii Petukhov <dimart.sp@gmail.com>
2014-07-03 20:03:40 +0400

    SongKickGeoLocation: delete old example

--- src/SongKick.GeoLocation/SimpleExampleOfUsingJP.fs
+++
@@ -1,9 +0,0 @@
-open FSharp.Data
-type FedoraServer = JsonProvider<"https://geoip.fedoraproject.org/city">
-
-[<EntryPoint>]
-let main argv = 
-    let date = FedoraServer.GetSample()
-    let city = date.City
-    printfn "%A" city
-    0
