f91884d45f01ee43ecf35e1b27f3707a54a974c0
Dmitrii Petukhov <dimart.sp@gmail.com>
2014-07-21 06:39:15 +0400

    SongKick.GeoLocation: improve refresh logic
    Now it saves last update time and updates gigs list
    only when 12 hours pass. (Even if Banshee has been closed).

--- src/SongKickGeoLocation/Banshee.SongKickGeoLocation/SongKickGeoLocationService.fs
+++ src/SongKickGeoLocation/Banshee.SongKickGeoLocation/SongKickGeoLocationService.fs
@@ -31,6 +31,7 @@ open System
 open Banshee.Sources
 open Banshee.Kernel
 open Banshee.ServiceStack
+open Banshee.Configuration
 
 open Banshee.SongKick.Recommendations
 open Banshee.SongKick.Search
@@ -41,8 +42,10 @@ open Banshee.SongKickGeoLocation.UI
 
 open Hyena
 
-type Service() as this =
-    let refresh_timeout = uint32 (1000 * 60 * 60 * 12) // 12 hours in ms
+type Service () =
+    let default_refresh_time = TimeSpan.FromHours (12.0)
+    let delay_refresh_time   = TimeSpan.FromMinutes (30.0)
+    let config_variable_name = "last_recommended_gigs_scan"
     let banshee_window = Service.GetBansheeWindow ()
 
     let mutable refresh_timeout_id = uint32 0
@@ -50,10 +53,8 @@ type Service() as this =
     static let gigs_source = new RecommendedGigsSource ()
 
     member x.Initialize () =
-        LocationProviderManager.Register this
-        ThreadAssist.SpawnFromMain (fun() ->
-            refresh_timeout_id <- Application.RunTimeout (refresh_timeout, x.RefreshRecommededGigs)
-            ServiceManager.Get<Banshee.Networking.Network>().StateChanged.AddHandler x.OnNetworkStateChanged)
+        x.UpdateRunTimeout () //someone should set RunTimeout for the first time, so we do it manually here
+        ServiceManager.Get<Banshee.Networking.Network>().StateChanged.AddHandler x.OnNetworkStateChanged
 
     member private x.GetRecommededGigs () =
         let gigs = new Results<Event> ()
@@ -69,17 +70,69 @@ type Service() as this =
         gigs
 
     member private x.RefreshRecommededGigs = new TimeoutHandler (fun () ->
-        if LocationProviderManager.HasProvider
-           && (ServiceManager.Get<Banshee.Networking.Network> ()).Connected
-        then
-            Hyena.Log.Debug ("Refreshing list of local gigs")
+        match
+           (LocationProviderManager.HasProvider,
+            (ServiceManager.Get<Banshee.Networking.Network> ()).Connected,
+            x.IsItTimeToRefresh ()) with
+        | (true, true, true) ->
+            Log.Debug ("Refreshing list of local gigs")
+            DatabaseConfigurationClient.Client.Set<DateTime> (config_variable_name, DateTime.Now)
             Scheduler.Schedule (new DelegateJob (fun () ->
                 let gigs = x.GetRecommededGigs ()
                 if gigs.Count <> 0 then
-                    x.NotifyUser(gigs)))
+                   x.NotifyUser(gigs)
+                DatabaseConfigurationClient.Client.Set<DateTime> (config_variable_name, DateTime.Now)
+                x.UpdateRunTimeout ()))
             true
+         | (_, false, true) -> // if it's time to refresh gigs but there is no internet connection
+                               // then we delay refresh for "delay_refresh_time"
+                               // but it will be ok if internet connection appears earlier! :)
+                               x.DelayRefresh()
+                               true
+         | _ -> true)
+
+    member private x.GetLastUpdateTime () =
+        try
+            DatabaseConfigurationClient.Client.Get<DateTime> (config_variable_name, DateTime.MinValue)
+        with
+            | :? FormatException as e ->
+                    Log.Warning (String.Format ("{0} is malformed, resetting to default value", config_variable_name))
+                    DatabaseConfigurationClient.Client.Set<DateTime> (config_variable_name, DateTime.MinValue)
+                    DateTime.MinValue
+
+    member private x.IsItTimeToRefresh () =
+        let last_update = x.GetLastUpdateTime ()
+        let past = DateTime.UtcNow.Subtract (last_update.ToUniversalTime())
+        past > default_refresh_time
+
+    member private x.GetNextRetryTime () =
+        let last_update = x.GetLastUpdateTime ()
+        if last_update = DateTime.MinValue then
+            // if this is the first time when user turn on GeoLocation
+            // or saved time was in incorrect format
+            // then we should try to update gigs right now
+            TimeSpan.FromSeconds (1.0)
         else
-            true)
+            let past = DateTime.UtcNow.Subtract (last_update.ToUniversalTime())
+            if past > default_refresh_time then
+                // if too much time has passed since last Banshee's start
+                // then we should try to update gigs right now
+                TimeSpan.FromSeconds (1.0)
+            else
+                default_refresh_time.Subtract (past)
+
+    member private x.UpdateRunTimeout () =
+        let retryTime = x.GetNextRetryTime ()
+        let t = (uint32) retryTime.TotalMilliseconds
+        Application.IdleTimeoutRemove (refresh_timeout_id) |> ignore
+        refresh_timeout_id <- Application.RunTimeout (t, x.RefreshRecommededGigs)
+        Log.Information ("Recommended gigs list will be updated in " + retryTime.Duration().ToString())
+
+    member private x.DelayRefresh () =
+        let t = (uint32) delay_refresh_time.TotalMilliseconds
+        Application.IdleTimeoutRemove (refresh_timeout_id) |> ignore
+        refresh_timeout_id <- Application.RunTimeout (t, x.RefreshRecommededGigs)
+        Log.Information ("Recommended gigs list will be updated in " + delay_refresh_time.Duration().ToString())
 
     member private x.IsItInUserCity (lat : float, long : float) =
         abs (lat  - LocationProviderManager.GetLatitude)  < 1.0 &&
@@ -123,7 +176,7 @@ type Service() as this =
             ThreadAssist.SpawnFromMain (fun () ->
                 Threading.Thread.Sleep 500
                 LocationProviderManager.RefreshGeoPosition ()
-                x.RefreshRecommededGigs.Invoke() |> ignore))
+                x.RefreshRecommededGigs.Invoke () |> ignore))
 
     member x.Dispose () =
         Application.IdleTimeoutRemove (refresh_timeout_id) |> ignore
@@ -131,8 +184,6 @@ type Service() as this =
 
     member x.ServiceName = Constants.NAME + ".Service"
 
-    interface ICityNameObserver with
-        member x.OnCityNameUpdated (cn : string) = x.RefreshRecommededGigs.Invoke() |> ignore
     interface IService with
         member x.ServiceName = x.ServiceName
     interface IDisposable with
